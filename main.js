d5aedd873f26c0d7c7b34826f1b5827c4fe51762
Electogenius <69585636+Electogenius@users.noreply.github.com>
2023-05-08 13:13:27 +0530

    better layout & settings & openInNewWindow

--- main.js
+++ main.js
@@ -1,18 +1,17 @@
 var e = ace.edit("editor");
 var settings = {
 	horizmode: false,
-	mode: "html",
 	preview: {
 		pre: "",
 		post: "",
 	},
 	live: true,
-	autosave: 3,
+	autosave: 1,
 	font: {
 		size: 12,
 		family: "monospace"
 	},
-	eruda: true,
+	eruda: false,
 }
 if (localStorage.getItem("settings") === null) {
 	localStorage.setItem("settings", JSON.stringify(settings))
@@ -21,46 +20,43 @@ if (localStorage.getItem("settings") === null) {
 }
 
 //pe.setTheme("ace/theme/" + settings.theme);
-e.session.setMode("ace/mode/" + settings.mode);
+e.session.setMode("ace/mode/html")
 
-window.addEventListener("DOMContentLoaded",()=>{
+window.addEventListener("DOMContentLoaded", () => {
 	let Emmet = require("ace/ext/emmet");
 	e.setOption("enableEmmet", true);
-})
+});
 
 function save() {
 	localStorage.setItem("code", e.getValue());
 	localStorage.setItem("editor", JSON.stringify(e.getOptions()))
 }
+save()
+
 function preview() {
 	let x = ""
-	if(settings.eruda)x = "<script src=\"https://cdnjs.cloudflare.com/ajax/libs/eruda/2.4.1/eruda.min.js\" onload=\"eruda.init()\"></script>"
-	i.srcdoc = x + settings.preview.pre + e.getValue() + settings.preview.post 
+	if (settings.eruda) x = "<script src=\"https://cdnjs.cloudflare.com/ajax/libs/eruda/2.4.1/eruda.min.js\" onload=\"eruda.init()\"></script>"
+	i.srcdoc = x + settings.preview.pre + e.getValue() + settings.preview.post
 }
 function openSettings() {
 	let s = {}
 	let f = document.createElement("form")
 	f.id = "set"
-	f.innerHTML = "<h1>Settings</h1><small>Remove their values to see what the inputs are for. If you don't want to save what you have entered then refresh the page. For more editor related settings, enter ctrl+',' or cmd+',' or on mobile, use the ... button, open palette and select settings</small>"
-	s.m = document.createElement("input") //language mode
-	s.m.value = settings.mode
-	s.m.placeholder = "language mode (html, javascript, etc.)"
-	s.m.oninput = e => settings.mode = e.target.value.toLowerCase()
-	f.appendChild(s.m)
-	
+	f.innerHTML = "<h1>Settings</h1><p>Remove the values to see what the inputs are for. If you don't want to save what you have entered then refresh the page.<br><br>For more editor related settings, enter ctrl+',' or cmd+',' or on mobile, use the ... button, open palette and select settings</p>"
+
 	s.as = document.createElement("input") //autosave
 	s.as.type = "number"
 	s.as.placeholder = "autosave delay (seconds) (0 = no autosave)"
 	s.as.value = settings.autosave
 	s.as.oninput = e => settings.autosave = e.target.value
 	f.appendChild(s.as)
-	
+
 	s.pr = document.createElement("textarea") //pre
 	s.pr.value = settings.preview.pre
 	s.pr.placeholder = "HTML code to add before the code entered in ace before it is shown in the preview (like \"<script>\")"
 	s.pr.oninput = e => settings.preview.pre = e.target.value
 	f.appendChild(s.pr)
-	
+
 	s.po = document.createElement("textarea") //post
 	s.po.value = settings.preview.post
 	s.po.placeholder = "HTML code to add after the code entered in ace before it is shown in the preview (like \"</script>\")"
@@ -85,9 +81,9 @@ function openSettings() {
 	s.v.oninput = e => settings.horizmode = e.target.checked
 	f.appendChild(s.v)
 	f.appendChild(s.l)
-	
+
 	f.appendChild(document.createElement("br"))
-	
+
 	s.v = document.createElement("input") //console
 	s.l = document.createElement("label")
 	s.v.checked = settings.eruda
@@ -107,7 +103,7 @@ function openSettings() {
 	s.v.oninput = e => settings.live = e.target.checked
 	f.appendChild(s.v)
 	f.appendChild(s.l)
-	
+
 	s.t = document.createElement("input") //theme
 	s.t.value = settings.font.family
 	s.t.placeholder = "css font family name"
@@ -147,7 +143,7 @@ if (localStorage.getItem("code") !== null) {
 	localStorage.setItem("code", def)
 	e.setValue(localStorage.getItem("code"))
 }
-localStorage.setItem("code","")
+localStorage.setItem("code", "")
 e.setOptions({
 	enableLiveAutocompletion: true,
 	fontFamily: settings.font.family,
@@ -160,15 +156,16 @@ e.on("change", () => {
 	}
 })
 if (settings.autosave) {
-	setTimeout(()=>setInterval(() => save(), settings.autosave * 1000), 1000)
+	setTimeout(() => setInterval(() => save(), settings.autosave * 1000), 1000)
 }
-if(settings.horizmode){
-	document.getElementById("editor").style.height = "90vh"
-	document.getElementById("editor").style.width = "49vw"
-	i.style.height = "90vh"
-	i.style.width = "49vw"
-	i.style.margin = "0"
-	i.style.marginLeft = "49vw"
+if (settings.horizmode) {
+	// document.getElementById("editor").style.height = "90vh"
+	// document.getElementById("editor").style.width = "49vw"
+	// i.style.height = "90vh"
+	// i.style.width = "49vw"
+	// i.style.margin = "0"
+	// i.style.marginLeft = "49vw"
+	document.body.className = "horizontal"
 }
 e.setFontSize(Number(settings.font.size))
 e.setOptions(JSON.parse(localStorage.getItem("editor")))
@@ -177,13 +174,13 @@ function full() {
 	let x = ""
 	if (settings.eruda) x = "<script src=\"https://cdn.jsdelivr.net/npm/eruda/eruda.js\" onload=\"eruda.init()\"></script>"
 	let htm = x + settings.preview.pre + e.getValue() + settings.preview.post
-	open("https://showhtm.vercel.app/?"+btoa(htm))
+	open("/compress.html?" + encodeURIComponent(htm))
 }
-setTimeout(()=>{
+setTimeout(() => {
 	e.setOptions({
 		enableLiveAutocompletion: true,
 		fontFamily: settings.font.family,
 		enableSnippets: true
 	});
-	localStorage.setItem("code",e.getValue())
-},500)
+	localStorage.setItem("code", e.getValue())
+}, 500)
